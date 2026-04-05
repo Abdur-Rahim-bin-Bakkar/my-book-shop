5c441170081fd650d6637374b66cffa8f1e2a6d8
webdesignrahim4061 <webdesignrahim4061@gmail.com>
2026-04-05 20:42:17 +0600

    project init and inistall tailwindcss and daisy UI

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>this is heading</h1>
+      <button className='btn btn-primary'>click me</button>
+    </div>
+  )
+}
+
+export default App
